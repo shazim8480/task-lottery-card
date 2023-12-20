662c31d324bf185979927f1ac8358b22a1d17676
shazim8480 <troublemaker8480@gmail.com>
2023-12-20 13:29:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="bg-blue-800">
+      <h1 className="text-slate-500"></h1>
+      hello vite
+    </main>
+  );
+}
+
+export default App;
